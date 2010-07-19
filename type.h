0d6f83b51cd27470c0ca28d2bbb89616b9707955
powerbombkun <powerbombkun@gmail.com>
2010-07-20 02:07:19 +0900

    [add] type.h macro.h

---
+++ type.h
@@ -0,0 +1,25 @@
+/**
+ * @file   type.h
+ * @author Junsei Takahashi
+ *
+ * @date   last update 2010-07-20 01:54:55
+ *
+ * @brief  プログラムの標準型定義
+ *
+ */
+#ifndef _TYPE_H
+#define _TYPE_H
+
+#define FALSE 0
+#define TRUE 1
+
+typedef int            bool_t;
+typedef unsigned char  byte_t;
+typedef unsigned char  uint8_t;
+typedef signed char    int8_t;
+typedef unsigned short uint16_t;
+typedef short          int16_t;
+typedef unsigned int   uint32_t;
+typedef int            int32_t;
+
+#endif /* _TYPE_H */
