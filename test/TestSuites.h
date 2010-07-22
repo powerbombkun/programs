f376849dad14121d8e99ef5e48a99f538121e360
powerbombkun <powerbombkun@gmail.com>
2010-07-23 00:26:57 +0900

    add test_ringbuffer.c

--- test/TestSuites.h
+++ test/TestSuites.h
@@ -1,7 +1,7 @@
 /**
  * @file   TestSuites.h
  * @author Junsei Takahashi
- * @date   last update 2010-07-23 00:13:21
+ * @date   last update 2010-07-23 00:25:34
  *
  * @brief 本ファイルで宣言したもののみを対象にテストを行う。
  */
@@ -14,16 +14,23 @@
 /**
  * util
  */
-extern CU_TestInfo test_util_array[];
-int setup_test_util();
-int teardown_test_util();
+/* extern CU_TestInfo test_util_array[]; */
+/* int setup_test_util(); */
+/* int teardown_test_util(); */
+/**
+ * ringbuffer
+ */
+extern CU_TestInfo test_ringbuffer_array[];
+int setup_test_ringbuffer();
+int teardown_test_ringbuffer();
 /**
  * テストスイート配列
  *
  * @brief テスト実行対象となるテストスイート
  */
 static CU_SuiteInfo suites_array[] = {
-   {"test_util", setup_test_util, teardown_test_util, test_util_array},
+   /* {"test_util", setup_test_util, teardown_test_util, test_util_array}, */
+   {"test_ringbuffer", setup_test_ringbuffer, teardown_test_ringbuffer, test_ringbuffer_array},
    CU_SUITE_INFO_NULL
 };
 #endif /* _TESTSUITES_H */
