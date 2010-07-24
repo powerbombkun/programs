b32046c2fdfaa66c628e506516334c926afe3227
powerbombkun <powerbombkun@gmail.com>
2010-07-24 11:57:33 +0900

    modify get_url.rb

--- script/get_url.rb
+++ script/get_url.rb
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 #
 #Author:: Junsei Takahashi
-#* last update 2010-07-24 11:54:28
+#* last update 2010-07-24 11:57:06
 #
 
 require 'open-uri'
 
-url = "http://twitter.com/powerbombkun"
+url = "http://www.google.co.jp/"
 open(url){|f| puts f.read }
