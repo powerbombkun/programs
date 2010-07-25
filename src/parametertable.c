241c25c773f6690e168220e3c5437e7c8f3ce80e
powerbombkun <powerbombkun@gmail.com>
2010-07-25 21:21:04 +0900

    add parametertable.h,c

---
+++ src/parametertable.c
@@ -0,0 +1,166 @@
+#include "parametertable.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DELIM           ": "
+#define MAX_BUFFER_SIZE 1024
+/**
+ * @struct data_map_t
+ * @brief キーと値のペアでの情報管理構造体
+ */
+typedef struct{
+    char*       key;            /** キー */
+    int32_t     val;            /** 値 */
+}data_map_t;
+/**
+ * @struct parametertable_t
+ * @brief オブジェクトの内部パラメーター
+ */
+typedef struct{
+    data_map_t* p_map;          /** data_map_t型ポインタ */
+    int32_t     n_map;          /** p_mapの個数 */
+}parametertable_t;
+
+
+ParameterTable_Handle ParameterTable_create()
+{
+    parametertable_t* This;
+    This = (parametertable_t*)malloc(sizeof(parametertable_t));
+
+    return (ParameterTable_Handle)This;
+}
+
+void      ParameterTable_delete(ParameterTable_Handle h_obj)
+{
+    parametertable_t* This = (parametertable_t*)h_obj;
+    ParameterTable_initialize(h_obj);
+    if(This     != NULL) free(This);
+}
+
+void      ParameterTable_initialize(ParameterTable_Handle h_obj)
+{
+    parametertable_t* This = (parametertable_t*)h_obj;
+    int               i;
+
+    for(i = 0;i < This->n_map;i++)
+    {
+        free(This->p_map[i].key);
+        free(&This->p_map[i]);
+    }
+    This->p_map = NULL;
+    This->n_map = 0;
+}
+
+int32_t    ParameterTable_entry(ParameterTable_Handle h_obj,
+                                const char*           key,
+                                int32_t               val)
+{
+    parametertable_t* This = (parametertable_t*)h_obj;
+    int32_t           ret  = FAILURE;
+
+    This->p_map[This->n_map].key = (char*)malloc(strlen(key) + 1);
+    This->p_map[This->n_map].val = val;
+    This->n_map++;
+
+    return ret;
+}
+
+int32_t    ParameterTable_setData(ParameterTable_Handle h_obj,
+                                  const char*           key,
+                                  int32_t               val)
+{
+    parametertable_t* This = (parametertable_t*)h_obj;
+    int               i;
+    int32_t           ret  = FAILURE;
+
+    for(i = 0;i < This->n_map;i++)
+    {
+        if(strcmp(This->p_map[i].key,key) == 0)
+        {
+            This->p_map[i].val = val;
+            ret                = SUCCESS;
+            break;
+        }
+    }
+    return ret;
+}
+
+int32_t    ParameterTable_getData(ParameterTable_Handle h_obj,
+                                  const char*           key,
+                                  int32_t*              val)
+{
+    parametertable_t* This = (parametertable_t*)h_obj;
+    int               i;
+    int32_t           ret  = FAILURE;
+
+    for(i = 0;i < This->n_map;i++)
+    {
+        if(strcmp(This->p_map[i].key,key) == 0)
+        {
+            *val = This->p_map[i].val;
+            ret  = SUCCESS;
+            break;
+        }
+    }
+    return ret;
+}
+
+int32_t    ParameterTable_writeFile(ParameterTable_Handle h_obj,
+                                    const char*           file)
+{
+    parametertable_t* This = (parametertable_t*)h_obj;
+    int               i;
+    int32_t           ret  = FAILURE;
+    FILE*             fp;
+    fp                     = fopen(file,"w");
+    if(fp != NULL)
+    {
+        /**
+         * - YAMLフォーマット出力
+         */
+        for(i = 0;i < This->n_map;i++)
+        {
+            printf("%s%s%d\n",This->p_map[i].key,DELIM,This->p_map[i].val);
+        }
+        fclose(fp);
+        ret = SUCCESS;
+    }
+    return ret;
+}
+
+int32_t    ParameterTable_readFile(ParameterTable_Handle h_obj,
+                                   const char*           file)
+{
+    parametertable_t* This = (parametertable_t*)h_obj;
+    int               i;
+    int32_t           ret  = FAILURE;
+    FILE*             fp;
+    fp                     = fopen(file,"r");
+    if(fp != NULL)
+    {
+        char buffer[MAX_BUFFER_SIZE];
+        while(fgets(buffer,sizeof(buffer),fp) != NULL)
+        {
+            /**
+             * - キーに対応する値をサーチして値を設定する
+             */
+            for(i = 0;i < This->n_map;i++)
+            {
+                if(strstr(buffer,This->p_map[i].key) != NULL)
+                {
+                    const char* p = strstr(buffer,DELIM);
+                    if(p != NULL)
+                    {
+                        p                  += strlen(DELIM);
+                        This->p_map[i].val  = atoi(p);
+                    }
+                }
+            }
+        }
+        fclose(fp);
+        ret = SUCCESS;
+    }
+    return ret;
+}
+
---
+++ src/parametertable.h
@@ -0,0 +1,113 @@
+/**
+ * @file   parametertable.h
+ * @author Junsei Takahashi
+ *
+ * @date   last update 2010-07-25 21:20:30
+ *
+ * @brief
+ *
+ */
+#ifndef _PARAMETERTABLE_H
+#define _PARAMETERTABLE_H
+#include "type.h"
+#include "macro.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef void* ParameterTable_Handle;
+    /**
+     * @brief パラメーターテーブルオブジェクトの構築
+     *
+     * @retval オブジェクトハンドル
+     */
+    ParameterTable_Handle ParameterTable_create();
+    /**
+     * @brief パラメーターテーブルオブジェクトの消滅
+     *
+     * @param[in] h_obj オブジェクトハンドル
+     *
+     */
+    void ParameterTable_delete(ParameterTable_Handle h_obj);
+    /**
+     * @brief パラメーターテーブルオブジェクトの初期化
+     *
+     * @param[in] h_obj オブジェクトハンドル
+     *
+     */
+    void ParameterTable_initialize(ParameterTable_Handle h_obj);
+    /**
+     * @brief 内部のパラメーターテーブルへ情報を登録します
+     *
+     * @param[in] h_obj オブジェクトハンドル
+     * @param[in] key   登録するキー
+     * @param[in] val   初期値
+     *
+     * @retval SUCCESS  成功
+     *         FAILED   失敗
+     */
+    int32_t    ParameterTable_entry(ParameterTable_Handle h_obj,
+                                    const char*           key,
+                                    int32_t               val);
+
+    /**
+     * @brief 内部のパラメーターテーブルへ値を設定する
+     *
+     * @param[in] h_obj オブジェクトハンドル
+     * @param[in] key   キー
+     * @param[in] val   値
+     *
+     * @retval SUCCESS  成功
+     *         FAILED   失敗
+     */
+    int32_t    ParameterTable_setData(ParameterTable_Handle h_obj,
+                                      const char*           key,
+                                      int32_t               val);
+
+    /**
+     * @brief 内部のパラメーターテーブルから値を取得する
+     *
+     * @param[in] h_obj オブジェクトハンドル
+     * @param[in] key   キー
+     * @param[out] val   値
+     *
+     * @retval SUCCESS  成功
+     *         FAILED   失敗
+     */
+    int32_t    ParameterTable_getData(ParameterTable_Handle h_obj,
+                                      const char*           key,
+                                      int32_t*              val);
+
+    /**
+     * @brief 内部のパラメーターをファイルへ出力します
+     *
+     * @param[in] h_obj オブジェクトハンドル
+     * @param[in] file  ファイルパス
+     *
+     * @retval SUCCESS  成功
+     *         FAILED   失敗
+     */
+    int32_t    ParameterTable_writeFile(ParameterTable_Handle h_obj,
+                                        const char*           file);
+
+    /**
+     * @brief パラメーターをファイルから読み込みます
+     *
+     * @param[in] h_obj オブジェクトハンドル
+     * @param[in] file  ファイルパス
+     *
+     * @retval SUCCESS  成功
+     *         FAILED   失敗
+     */
+    int32_t    ParameterTable_readFile(ParameterTable_Handle h_obj,
+                                       const char*           file);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+#endif                                  /* _PARAMETERTABLE_H */
