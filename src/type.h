4734c29710ff088f6b486e240fbc9793990166bd
powerbombkun <powerbombkun@gmail.com>
2010-08-04 20:53:08 +0900

    modify type.h add C99 ifdef option

--- src/type.h
+++ src/type.h
@@ -2,7 +2,7 @@
  * @file   type.h
  * @author Junsei Takahashi
  *
- * @date   last update 2010-07-30 20:45:56
+ * @date   last update 2010-08-04 20:46:35
  *
  * @brief  プログラムの標準型定義
  *
@@ -10,14 +10,16 @@
 #ifndef _TYPE_H
 #define _TYPE_H
 
-#ifndef _STDINT_H
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
+#include <stdint.h>
+#else
 typedef unsigned char  uint8_t;
 typedef signed char    int8_t;
 typedef unsigned short uint16_t;
 typedef short          int16_t;
 typedef unsigned int   uint32_t;
 typedef int            int32_t;
-#endif
+#endif /* defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) */
 
 #ifndef BOOL
 #define BOOL int
