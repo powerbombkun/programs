b9ede5805a503844ce318089c73cf64bb4282d5e
powerbombkun <powerbombkun@gmail.com>
2010-08-05 23:03:42 +0900

    add cygwin dir

--- src/type.h
+++ src/type.h
@@ -2,7 +2,7 @@
  * @file   type.h
  * @author Junsei Takahashi
  *
- * @date   last update 2010-08-04 20:46:35
+ * @date   last update 2010-08-05 23:02:45
  *
  * @brief  プログラムの標準型定義
  *
@@ -10,16 +10,15 @@
 #ifndef _TYPE_H
 #define _TYPE_H
 
-#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
 #include <stdint.h>
-#else
+#ifndef _STDINT_H
 typedef unsigned char  uint8_t;
 typedef signed char    int8_t;
 typedef unsigned short uint16_t;
 typedef short          int16_t;
 typedef unsigned int   uint32_t;
 typedef int            int32_t;
-#endif /* defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) */
+#endif
 
 #ifndef BOOL
 #define BOOL int
