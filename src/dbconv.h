0aab5441021c69e2c0f0e1930c21ba4dd1195224
powerbombkun <powerbombkun@gmail.com>
2010-08-05 23:22:01 +0900

    modify dbconv.h,c add c++ option

--- src/dbconv.h
+++ src/dbconv.h
@@ -2,7 +2,7 @@
  * @file   dbconv.h
  * @author Junsei Takahashi
  *
- * @date   last update 2010-08-05 23:16:24
+ * @date   last update 2010-08-05 23:21:33
  *
  * @brief
  *
@@ -11,22 +11,30 @@
 #define _DBCONV_H
 #include "type.h"
 #include "macro.h"
-/**
- * @brief 符号付き16bitのリニアデータをデシベル値に変換します。
- *        デシベル値に関しては符号付き16bitリニアデータの最大値を0dBとして計算。
- *
- * @param[in] linear  リニアデータ
- *
- * @retval デシベル値
- */
-float linear2db(int16_t linear);
-/**
- * @brief デシベル値を符号付き16bitリニアデータに変換します。
- *
- * @param[in] db  デシベル値
- *
- * @retval リニアデータ
- */
-int16_t db2linear(float db);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    /**
+     * @brief 符号付き16bitのリニアデータをデシベル値に変換します。
+     *        デシベル値に関しては符号付き16bitリニアデータの最大値を0dBとして計算。
+     *
+     * @param[in] linear  リニアデータ
+     *
+     * @retval デシベル値
+     */
+    float linear2db(int16_t linear);
+    /**
+     * @brief デシベル値を符号付き16bitリニアデータに変換します。
+     *
+     * @param[in] db  デシベル値
+     *
+     * @retval リニアデータ
+     */
+    int16_t db2linear(float db);
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
