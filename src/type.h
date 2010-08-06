8e1cc1e28c546826ea64dc4df5a5bbf18984b40c
powerbombkun <powerbombkun@gmail.com>
2010-08-06 22:59:03 +0900

    type.h modify

--- src/type.h
+++ src/type.h
@@ -2,7 +2,7 @@
  * @file   type.h
  * @author Junsei Takahashi
  *
- * @date   last update 2010-08-05 23:02:45
+ * @date   last update 2010-08-06 22:58:30
  *
  * @brief  プログラムの標準型定義
  *
@@ -10,7 +10,9 @@
 #ifndef _TYPE_H
 #define _TYPE_H
 
+#ifndef _WIN32
 #include <stdint.h>
+#endif
 #ifndef _STDINT_H
 typedef unsigned char  uint8_t;
 typedef signed char    int8_t;
