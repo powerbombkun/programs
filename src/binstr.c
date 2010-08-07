9aa775af3f5ca45f78ddb59b2783d5c1b74f5e4c
powerbombkun <powerbombkun@gmail.com>
2010-08-07 13:20:26 +0900

    [update] strbin.c - str2bin : strtol func use

--- src/binstr.c
+++ src/binstr.c
@@ -37,35 +37,14 @@ int32_t bin2str(const uint8_t* p_bin,int32_t n_bin,char* p_buffer,int32_t n_buff
 
 int32_t str2bin(const char* p_str,int32_t len,uint8_t* p_buffer,int32_t n_buffer)
 {
-    int i,j;
+    int i;
     int32_t ret = FAILURE;
     if(n_buffer >= (len/2))
     {
         for(i = 0;i < (len/2);i++)
         {
-            int code[2];
-            for(j = 0;j < 2;j++)
-            {
-                if('A' <= *p_str && *p_str <= 'F')
-                {
-                    code[j] = *p_str - 'A' + 10;
-                }
-                else if('a' <= *p_str && *p_str <= 'f')
-                {
-                    code[j] = *p_str - 'a' + 10;
-                }
-                else if('0' <= *p_str && *p_str <= '9')
-                {
-                    code[j] = *p_str - '0';
-                }
-                else
-                {
-                    /** その他の文字に関しては0で埋める */
-                    code[j] = 0x00;
-                }
-                p_str++;
-            }
-            *p_buffer++ = (uint8_t)(code[TOP]*16+code[BOTTOM]);
+            char s[] = {p_str[2*i],p_str[2*i+1]};
+            *p_buffer++ = strtol(s,NULL,16);
         }
         ret = SUCCESS;
     }
