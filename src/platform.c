bce126e552b9f50e5a5372100bbf19eff0c06977
powerbombkun <powerbombkun@gmail.com>
2010-08-09 20:19:22 +0900

    [modify] add argv void option

--- src/platform.c
+++ src/platform.c
@@ -1,10 +1,10 @@
 #include "platform.h"
 
-int int_bits()
+int int_bits(void)
 {
     int count = 0;
     unsigned int a =~ 0;
-    while(a & 1)
+    while(a & 1U)
     {
         count++;
         a>>=1;
@@ -12,7 +12,7 @@ int int_bits()
     return count;
 }
 
-int is_little_endian()
+int is_little_endian(void)
 {
     int32_t a = 1;
     return *(uint8_t*)&a;
