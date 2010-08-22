6a352e431b92253f60178b64255fc749383149cb
powerbombkun <powerbombkun@gmail.com>
2010-08-22 14:55:02 +0900

    [update] xfft.c - int -> int32_t type changed

--- src/xfft.c
+++ src/xfft.c
@@ -6,8 +6,8 @@ void xfft(int16_t* p_data,int32_t n_data,double* re,double* im,int32_t     bitsi
 {
     int i;
     int j;
-    int datasize = 1 << bitsize;
-    int n_loop   = n_data / datasize;
+    int32_t datasize = 1 << bitsize;
+    int32_t n_loop   = n_data / datasize;
     for(i = 0;i < n_loop;i++)
     {
         for(j = 0;j < datasize;j++)
@@ -27,8 +27,8 @@ void xifft(double* re,double* im,int16_t* p_buffer,int32_t n_buffer,int32_t     
 {
     int i;
     int j;
-    int datasize = 1 << bitsize;
-    int n_loop   = n_buffer / datasize;
+    int32_t datasize = 1 << bitsize;
+    int32_t n_loop   = n_buffer / datasize;
     for(i = 0;i < n_loop;i++)
     {
         ifft(re,im,bitsize);
@@ -46,8 +46,8 @@ int32_t xspectrum(int16_t* p_data,int32_t n_data,double* p_spectrum,int32_t     
 {
     int     i;
     int     j;
-    int32_t ret = FAILURE;
-    int     datasize = 1 << bitsize;
+    int32_t ret      = FAILURE;
+    int32_t datasize = 1 << bitsize;
     double* re       = (double*)malloc(datasize * sizeof(double));
     double* im       = (double*)malloc(datasize * sizeof(double));
     if((re != NULL) && (im != NULL))
