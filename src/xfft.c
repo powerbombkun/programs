2d6d866daa0b656fd585c8112c3df0cc9d1d0f04
powerbombkun <powerbombkun@gmail.com>
2010-08-23 23:09:10 +0900

    [modify] xfft.h,c - add window func

--- src/xfft.c
+++ src/xfft.c
@@ -2,6 +2,61 @@
 #include <stdlib.h>
 #include "fft.h"
 
+/**
+ * @typedef fft_window_t型enum
+ *
+ * @brief 窓掛け用の窓のID
+ */
+typedef enum
+{
+    HAMMING,                    /** ハミング窓 */
+    BLACKMAN,                   /** ブラックマン窓 */
+    HANNING,                    /** ハニング窓 */
+    NONE,
+} fft_window_t;
+/**
+ * @brief 回転子を取得する関数
+ *
+ * @param[in] bitsize   処理ビットサイズ
+ *
+ */
+static double getW(int32_t datasize);
+/**
+ * @brief 窓関数の係数を取得する関数
+ *
+ * @param[in] type 窓掛けを行う窓の種類
+ * @param[in] w 回転子
+ * @param[in] i インデックス
+ *
+ */
+static double getWindowRate(fft_window_t type,double w,int32_t i);
+
+static double getW(int32_t datasize)
+{
+    return (2.0 * PI / (double)datasize);
+}
+
+static double getWindowRate(fft_window_t type,double w,int32_t i)
+{
+    double rate;
+    if(type == HAMMING)
+    {
+        rate = 0.54 - (0.46 * (double)cos(w*i));
+    }
+    else if(type == BLACKMAN)
+    {
+        rate = 0.42 - (0.5 * (double)cos(w*i)) + (0.08 * (double)cos(2.0*w*i));
+    }
+    else if(type == HANNING)
+    {
+        rate = 0.5 - (0.5 * (double)cos(w*i));
+    }
+    else
+    {
+        rate = 1.0;
+    }
+    return rate;
+}
 
 void xfft(int16_t* p_data,int32_t n_data,double* re,double* im,int32_t     bitsize)
 {
