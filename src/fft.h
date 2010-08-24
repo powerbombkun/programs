c1669d508b4d434050d6b4950ffe9886751e7835
powerbombkun <powerbombkun@gmail.com>
2010-08-25 01:08:06 +0900

    [test] test commit vc.el

--- src/fft.h
+++ src/fft.h
@@ -2,7 +2,7 @@
  * @file   fft.h
  * @author Junsei Takahashi
  *
- * @date   last update 2010-08-25 01:02:08
+ * @date   last update 2010-08-25 01:07:24
  *
  * @brief
  *
@@ -15,6 +15,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
     /**
      * @brief FFT処理を行う関数
      *        1 << bitsize 分のデータがre,imそれぞれで必要
